b9fd537f39a2bc5abce0864652c96ac787aa2e3c
John Morrow <CodeMorrow@yahoo.com>
2016-08-22 10:15:01 -0500

    add logo , edit layout

--- wp-content/themes/customizable/footer.php
+++ wp-content/themes/customizable/footer.php
@@ -6,35 +6,30 @@
 <footer class="main_footer">
   <div class="top_footer footer">
     <div class="container customize-container">
-      <!--Link to My Facebook Page-->    
-    <div id="facebook" class="footer__link">
-        <a class="social_media" href="https://www.facebook.com/JMorrow16/" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/facebook.png" alt="Facebook"></a>
-    </div>
-    
-    <!--Link to My Twitter Page-->
-    <div id="twitter" class="footer__link">
-        <a class="social_media" href="https://twitter.com/Code_Morrow" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/twitter.png" alt="Twitter"></a>
-     </div>
-    
-    <!--Link to My Github Page-->
-    <div id="github" class="footer__link">
-        <a class="social_media" href="https://github.com/CodeMorrow" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/github-white.svg" alt="Github"></a>
-    </div>
+        <!--Link to My Facebook Page-->    
+      <div id="facebook" class="footer__link">
+          <a class="social_media" href="https://www.facebook.com/JMorrow16/" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/facebook.png" alt="Facebook"></a>
+      </div>
+      
+      <!--Link to My Twitter Page-->
+      <div id="twitter" class="footer__link">
+          <a class="social_media" href="https://twitter.com/Code_Morrow" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/twitter.png" alt="Twitter"></a>
+       </div>
+      
+      <!--Link to My Github Page-->
+      <div id="github" class="footer__link">
+          <a class="social_media" href="https://github.com/CodeMorrow" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/github-white.svg" alt="Github"></a>
+      </div>
 
-    <!--Link to My LinkedIn Page-->
-    <div id="linkedin" class="footer__link">
-        <a class="social_media" href="https://www.linkedin.com/in/john-morrow-758545111" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/linkedin.png" alt="LinkedIn"></a>
-    </div>
+      <!--Link to My LinkedIn Page-->
+      <div id="linkedin" class="footer__link">
+          <a class="social_media" href="https://www.linkedin.com/in/john-morrow-758545111" target="_blank"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/linkedin.png" alt="LinkedIn"></a>
+      </div>
 
-    <!--Link to E-mail me-->
-    <div id="email" class="footer__link">
-      <a href="mailto:codemorrow@yahoo.com?Subject=Hey%20John,%20I%20checked%20out%20your%20website%20and%20wanted%20to%20reach%20out" target="_top"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/email.png" alt="Email"></a>
-    </div>
-
-    <!--Theme Switcher-->
-    <div id="theme" class="footer__link">
-      <a href="?style=pink" ><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/ribbon.png" alt="Breast Cencer Awareness Style Theme"></a>
-    </div>
+      <!--Link to E-mail me-->
+      <div id="email" class="footer__link">
+        <a href="mailto:codemorrow@yahoo.com?Subject=Hey%20John,%20I%20checked%20out%20your%20website%20and%20wanted%20to%20reach%20out" target="_top"><img class="footer__link__img" src="http://localhost:8888/wp-personal_website/wp-content/uploads/2016/08/email.png" alt="Email"></a>
+      </div>
     </div>
   </div>
   <div class="bottom_footer">
